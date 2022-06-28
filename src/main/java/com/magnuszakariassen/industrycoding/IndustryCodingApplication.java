0e756edfa9a93f399f15255e45c26a5f7da8972c
Magnus <magnuszaka@gmail.com>
2022-06-28 14:18:17 +0200

    initial commit

---
+++ src/main/java/com/magnuszakariassen/industrycoding/IndustryCodingApplication.java
@@ -0,0 +1,13 @@
+package com.magnuszakariassen.industrycoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustryCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustryCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magnuszakariassen/industrycoding/IndustryCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magnuszakariassen.industrycoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustryCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
